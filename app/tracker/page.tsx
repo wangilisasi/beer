bfe269660923e749a6fd2a2b77e60a126c1905a5
Emil Patrick <wangilisasi@gmail.com>
2025-07-03 21:37:43 +0200

    monkey

--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -124,7 +124,7 @@ export default function TrackerPage() {
   const formatCurrency = (amount: number) => {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'EUR',
     }).format(amount);
   };
 
